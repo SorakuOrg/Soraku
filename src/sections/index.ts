f57c08eacee75c32ee1530dd9e531a14e42bb61f
Adinfauzan <echo.adinfauzan@gmail.com>
2026-02-23 00:04:24 +0700

    fix: remove old vite/pages router files

--- src/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Hero';
-export * from './Features';
-export * from './Events';
-export * from './Blog';
-export * from './Discord';
-export * from './Testimonials';
-export * from './CTA';
--- src/stores/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './authStore';
-export * from './themeStore';
-export * from './uiStore';
-export * from './blogStore';
-export * from './eventStore';
